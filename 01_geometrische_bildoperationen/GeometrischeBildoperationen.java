e8a365d5de29d69bf6f6e51e70bd1fa3e5f599ff
InformatikHub <uli.goeppert@kgt.de>
2021-02-26 12:19:55 +0100

    Anmerkungen AB1, drehe links und rechts

--- 01_geometrische_bildoperationen/GeometrischeBildoperationen.java
+++ 01_geometrische_bildoperationen/GeometrischeBildoperationen.java
@@ -6,16 +6,16 @@ import java.util.Random;
  * Algorithmen zur Änderung der Pixelpositionen eines Pictures
  * z.B. drehen, spiegeln usw.
  *
- * @author Thomas Schaller
- * @author Uli Göppert
- * @version 1.1 (28.11.2019)
- */
+ * @author Clemens Schulmeister
+ * @version 1.1
 public class GeometrischeBildoperationen 
 {
     /** spiegeleHorizontal spiegelt das Bild, so dass rechts und links getauscht werden
      * @param originalbild Ein Bild (Picture), das gespiegelt werden soll
      * @return Eine gespiegelte Kopie des Bildes
      */
+
+//die anderen Methoden nicht löschen, brauchen wir alle später noch!
 
     public  Picture dreheRechts(Picture originalbild) {
         int breite = originalbild.getWidth();
@@ -26,7 +26,7 @@ public class GeometrischeBildoperationen
 
         for(int x=0; x < hoehe; x++) {
             for(int y=0;y < breite; y++) {
-                pixelNeu[x][y] = pixel[(breite-1)-y][(hoehe-1)-x];
+                pixelNeu[x][y] = pixel[(breite-1)-y][(hoehe-1)-x];// noch nicht ganz richtig, da du einmal zuviel von oben nach unten spiegelst.
             }
         }
 
@@ -43,7 +43,7 @@ public class GeometrischeBildoperationen
 
         for(int x=0; x < hoehe; x++) {
             for(int y=0;y < breite; y++) {
-                pixelNeu[x][y] = pixel[y][x];
+                pixelNeu[x][y] = pixel[y][x];//auch nicht richtig, kannst du das Ergebnis nicht prüfen?
             }
         }
 

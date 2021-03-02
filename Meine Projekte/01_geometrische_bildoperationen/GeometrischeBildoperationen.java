9eae59a6f91b3a8cd00f64b279d380e0a9b5a562
dragonfire479 <78789271+dragonfire479@users.noreply.github.com>
2021-03-02 11:00:00 +0100

    schwarz_weis

--- Meine Projekte/01_geometrische_bildoperationen/GeometrischeBildoperationen.java
+++ Meine Projekte/01_geometrische_bildoperationen/GeometrischeBildoperationen.java
@@ -66,6 +66,28 @@ public class GeometrischeBildoperationen
         neuesBild.setPixelArray(pixelNeu); 
         return neuesBild;
     }
+    public  Picture Schwarz_weiß_convert(Picture originalbild) {
+        int breite = originalbild.getWidth();
+        int hoehe  = originalbild.getHeight();
+
+        Color[][] pixel = originalbild.getPixelArray();
+        Color[][] pixelNeu = new Color[breite][hoehe];
+        int c;
+        Color farbe;
+
+        for(int x=0; x < breite; x++) {
+            for(int y=0;y < hoehe; y++) {
+                c = (pixel[x][y].getBlue() + pixel[x][y].getGreen() + pixel[x][y].getRed()) / 3;
+                farbe = new Color(c,c,c);
+                pixelNeu[x][y] = farbe;
+            }
+        }
+
+        Picture neuesBild = new Picture();
+        neuesBild.setPixelArray(pixelNeu); 
+        return neuesBild;
+    }
+
 
 
 }

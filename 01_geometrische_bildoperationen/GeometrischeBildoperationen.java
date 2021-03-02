936584afbaba8b5c2da5953f5c908f7278bb3c66
niniddee <78789331+niniddee@users.noreply.github.com>
2021-03-02 10:22:10 +0100

    drehenkomplett=180°

--- 01_geometrische_bildoperationen/GeometrischeBildoperationen.java
+++ 01_geometrische_bildoperationen/GeometrischeBildoperationen.java
@@ -34,7 +34,25 @@ public class GeometrischeBildoperationen
         return neuesBild;
     }
 
-    
+    public  Picture drehekomplett(Picture originalbild) {
+        int breite = originalbild.getWidth();
+        int  hoehe = originalbild.getHeight();
+
+        Color[][] pixel = originalbild.getPixelArray();
+        Color[][] pixelNeu = new Color[breite][hoehe];
+      
+
+        for(int x=0; x < breite; x++) {
+            for(int y=0;y < hoehe; y++) {
+                pixelNeu[x][y] = pixel[(breite-1)-x][(hoehe-1)-y];
+            }
+        }
+        
+
+        Picture neuesBild = new Picture();
+        neuesBild.setPixelArray(pixelNeu); 
+        return neuesBild;
+    }
 
 
 }

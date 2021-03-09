d5438b6e41f59c51f8d2bee13e8a7592ebe85b0f
dragonfire479 <78789271+dragonfire479@users.noreply.github.com>
2021-03-09 10:16:24 +0100

    color_methoden

--- Meine Projekte/01_geometrische_bildoperationen/Punktoperationen.java
+++ Meine Projekte/01_geometrische_bildoperationen/Punktoperationen.java
@@ -54,6 +54,67 @@ public class Punktoperationen
         neuesBild.setPixelArray(pixelNeu); 
         return neuesBild;
     }
+    public  Picture Schwarz_weiß_max (Picture originalbild) {
+        int breite = originalbild.getWidth();
+        int hoehe  = originalbild.getHeight();
+
+        Color[][] pixel = originalbild.getPixelArray();
+        Color[][] pixelNeu = new Color[breite][hoehe];
+        int c;
+        Color farbe;
+
+        for(int x=0; x < breite; x++) {
+            for(int y=0;y < hoehe; y++) {
+                c = pixel[x][y].getBlue(); 
+                if (c < pixel[x][y].getGreen()){
+                    c = pixel[x][y].getGreen();
+                }
+                if (c < pixel[x][y].getRed()){
+                    c = pixel[x][y].getRed();
+                }
+                if (c < pixel[x][y].getBlue()){
+                    c = pixel[x][y].getBlue();
+                }
+                farbe = new Color(c,c,c);
+                pixelNeu[x][y] = farbe;
+            }
+        }
+
+        Picture neuesBild = new Picture();
+        neuesBild.setPixelArray(pixelNeu); 
+        return neuesBild;
+    }
+    
+    public  Picture Schwarz_weiß_min (Picture originalbild) {
+        int breite = originalbild.getWidth();
+        int hoehe  = originalbild.getHeight();
+
+        Color[][] pixel = originalbild.getPixelArray();
+        Color[][] pixelNeu = new Color[breite][hoehe];
+        int c;
+        Color farbe;
+
+        for(int x=0; x < breite; x++) {
+            for(int y=0;y < hoehe; y++) {
+                c = pixel[x][y].getBlue(); 
+                if (c > pixel[x][y].getGreen()){
+                    c = pixel[x][y].getGreen();
+                }
+                if (c > pixel[x][y].getRed()){
+                    c = pixel[x][y].getRed();
+                }
+                if (c > pixel[x][y].getBlue()){
+                    c = pixel[x][y].getBlue();
+                }
+                farbe = new Color(c,c,c);
+                pixelNeu[x][y] = farbe;
+            }
+        }
+
+        Picture neuesBild = new Picture();
+        neuesBild.setPixelArray(pixelNeu); 
+        return neuesBild;
+    }
     private int runden(double value) {
       return (int) value;
    }

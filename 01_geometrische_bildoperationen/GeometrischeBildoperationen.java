36cde266386f4b4af3f5470ca0b06d41b88d6405
timp10 <timon.a.roeck@icloud.com>
2021-03-09 10:21:42 +0100

    Datum

--- 01_geometrische_bildoperationen/GeometrischeBildoperationen.java
+++ 01_geometrische_bildoperationen/GeometrischeBildoperationen.java
@@ -33,7 +33,36 @@ public class GeometrischeBildoperationen
         Picture neuesBild = new Picture();
         neuesBild.setPixelArray(pixelNeu); 
         return neuesBild;
+    } 
+    
+    public  Picture dreheLinks(Picture originalbild) {
+        int breite = originalbild.getHeight();
+        int hoehe  = originalbild.getWidth();
+
+        Color[][] pixel = originalbild.getPixelArray();
+        Color[][] pixelNeu = new Color[breite][hoehe];
+
+        for(int x=0; x < breite; x++) {
+            for(int y=0;y < hoehe; y++) {
+                pixelNeu[x][y] = pixel[(hoehe-1)-y][x];
+            }
+        }
+
+        Picture neuesBild = new Picture();
+        neuesBild.setPixelArray(pixelNeu); 
+        return neuesBild;
     }
+    
+    /** drehe180 dreh das bild um 180°
+     * @param originalbild Ein Bild (Picture), das gespiegelt werden soll
+     * @return Eine gespiegelte Kopie des Bildes
+     */
+
+    public  Picture drehe180(Picture originalbild) {
+        Picture bild90 = dreheLinks(originalbild);
+        Picture bild180 = dreheLinks(bild90);
+        return bild180;
+    } 
 
     
 

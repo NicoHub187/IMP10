23a31c159d73934c23ecbfb8bf697d93d22b2860
InformatikHub <uli.goeppert@kgt.de>
2021-03-09 10:23:28 +0100

    Punktoperationen Klasse erstellt

---
+++ 01_geometrische_bildoperationen/Punktoperationen.java
@@ -0,0 +1,36 @@
+import imp.*;
+import java.awt.Color;
+import java.util.Random;
+
+/**
+ * Beschreiben Sie hier die Klasse Punktoperationen.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Punktoperationen
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int x;
+
+    /**
+     * Konstruktor für Objekte der Klasse Punktoperationen
+     */
+    public Punktoperationen()
+    {
+        // Instanzvariable initialisieren
+        x = 0;
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        return x + y;
+    }
+}

206171e6879a2bfa54130ca7ea3a2a6db8647ee8
timp10 <timon.a.roeck@icloud.com>
2021-03-09 10:40:47 +0100

    datum 9.3

---
+++ 01_geometrische_bildoperationen/Punktoperationen.java
@@ -0,0 +1,39 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Punktoperationen.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Punktoperationen
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int x;
+
+    /**
+     * Konstruktor für Objekte der Klasse Punktoperationen
+     */
+    public Punktoperationen()
+    {
+        // Instanzvariable initialisieren
+        x = 0;
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        return x + y;
+    }
+    
+    public Picture graustufenDurchschnitt(Picture originalbild)
+    {
+        // 
+        return x + y;
+    }
+}

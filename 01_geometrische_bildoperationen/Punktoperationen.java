a809c7c7025fd71332e8a5678807807d9e7c94a7
QuiRoGit <timo.w.80@web.de>
2021-03-23 09:54:11 +0100

    graustufe max fertig + Anfang Natürlich

--- 01_geometrische_bildoperationen/Punktoperationen.java
+++ 01_geometrische_bildoperationen/Punktoperationen.java
@@ -75,10 +75,37 @@ public class Punktoperationen
         for(int x=0; x < breite; x++) {
             for(int y=0;y < hoehe; y++) {
               color = pixel [x][y].getBlue();
-              //noch nicht fertig :)
+              if (color > pixel [x][y].getRed()){
+                 color = pixel[x][y].getRed(); 
+              }
+              if (color > pixel [x][y].getGreen()){
+                 color = pixel[x][y].getGreen(); 
+              }
+              if (color > pixel [x][y].getBlue()){
+                 color = pixel[x][y].getBlue(); 
+              }
               farbe = new Color (color,color,color);
               pixelNeu[x][y] = farbe;
             }
+        }
+        Picture neuesBild = new Picture();
+        neuesBild.setPixelArray(pixelNeu); 
+        return neuesBild;
+    }
+    public Picture graustufenNatuerlich (Picture originalbild){
+        int breite = originalbild.getWidth();
+        int hoehe  = originalbild.getHeight();
+
+        Color[][] pixel = originalbild.getPixelArray();
+        Color[][] pixelNeu = new Color[breite][hoehe];
+        int color;
+        Color farbe;
+        for(int x=0; x < breite; x++) {
+          for(int y=0;y < hoehe; y++) {
+            color = pixel[x][y].getRed()*0.299 + pixel[x][y].getGreen()*0.587 + pixel[x][y].getBlue()*0.114; 
+            farbe = new Color (color,color,color);
+            pixelNeu[x][y] = farbe;  
+          }
         }
         Picture neuesBild = new Picture();
         neuesBild.setPixelArray(pixelNeu); 

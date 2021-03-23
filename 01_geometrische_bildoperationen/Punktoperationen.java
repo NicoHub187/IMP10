63a981fa5b90fa9c12d7cb7a306af720af5af54c
timp10 <timon.a.roeck@icloud.com>
2021-03-23 11:01:24 +0100

    Graustufen(punktoperationen)

--- 01_geometrische_bildoperationen/Punktoperationen.java
+++ 01_geometrische_bildoperationen/Punktoperationen.java
@@ -1,3 +1,4 @@
+
 import imp.*;
 import java.awt.Color;
 import java.util.Random;
@@ -19,7 +20,7 @@ public class Punktoperationen
     {
         // Instanzvariable initialisieren
         x = 0;
-    }
+  }
 
     /**
      * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
@@ -31,16 +32,55 @@ public class Punktoperationen
     {
         // tragen Sie hier den Code ein
         return 100 + 50;
-    }
+  }
     
-    public Picture Schawarz_weiß_convert (Picture originalbild){
-        int breite = originalbild
+    
+    
+        public Picture graustufenDurchschnitt(Picture originalbild){
+        int breite = originalbild.getWidth();
+        int hoehe = originalbild.getHeight();
         
-         
+        Color[][] pixel = originalbild.getPixelArray();
+        Color[][] pixelNeu = new Color[breite][hoehe];
+        int c;
+        
+        for(int x=0; x < breite; x++) {
+            for(int y=0;y < hoehe; y++) {
+                c = (int) (pixel[x][y].getRed()+ pixel[x][y].getGreen()+ pixel[x][y].getBlue())/ 3;
+                pixelNeu[x][y] = new Color(c,c,c);
+            }
+        }
+         Picture neuesBild = new Picture();
+        neuesBild.setPixelArray(pixelNeu); 
+        return neuesBild;  
+        
+        
+  }
     
-
-    
+     public Picture graustufenMin(Picture originalbild){
+        int breite = originalbild.getWidth();
+        int hoehe = originalbild.getHeight();
+        
+        Color[][] pixel = originalbild.getPixelArray();
+        Color[][] pixelNeu = new Color[breite][hoehe];
+        int c;
+        
+        for(int x=0; x < breite; x++) {
+           for(int y=0;y < hoehe; y++) {
+                c = pixel [x][y].getRed();
+                if (c > pixel[x][y].getGreen()) c = pixel [x][y].getGreen();
+                if (c > pixel[x][y].getBlue()) c = pixel [x][y].getBlue();
+                pixelNeu[x][y] = new Color(c,c,c);
+        }
     }
-    }
-} 
-
+         Picture neuesBild = new Picture();
+        neuesBild.setPixelArray(pixelNeu); 
+        return neuesBild;  
+        
+        
+  }
+  
+  
+  
+  
+}

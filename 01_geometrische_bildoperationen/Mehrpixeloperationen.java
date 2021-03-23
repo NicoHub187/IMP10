940e590a0a77aa39e3d8983e62450c4a3b39427b
ClemensHub <78789319+ClemensHub@users.noreply.github.com>
2021-03-23 10:18:01 +0100

    weich- und relieffilter

--- 01_geometrische_bildoperationen/Mehrpixeloperationen.java
+++ 01_geometrische_bildoperationen/Mehrpixeloperationen.java
@@ -9,57 +9,101 @@ import java.awt.Color;
  * @version (eine Versionsnummer oder ein Datum)
  */
 public class Mehrpixeloperationen
-{
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+{   public Picture relief(Picture originalbild) {
+        double filter [][] = new double [3][3];
 
-    /**
-     * Konstruktor für Objekte der Klasse Mehrpixeloperationen
-     */
-    public  Picture faltung(Picture originalbild//,double filter 
-    )
-    {   int filter [][] = new int [3][3]; //das ist der Filter
         filter [0][0] = -1;
-        filter [1][0] = -2;
+        filter [1][0] = 1;
         filter [2][0] = -1;
-        filter [0][1] = 0;
-        filter [1][1] = 0;
-        filter [2][1] = 0;
-        filter [0][2] = 1;
-        filter [1][2] = 2;
-        filter [2][2] = 1;
+        filter [0][1] = 1;
+        filter [1][1] = 1;
+        filter [2][1] = 1;
+        filter [0][2] = -1;
+        filter [1][2] = 1;
+        filter [2][2] = -1;
 
-        Color[][] pixel = originalbild.getPixelArray();
-        Color[][] pixelNeu = originalbild.getPixelArray();
-        int laenge = filter.length;
-        int halb = laenge / 2;
-        double rot = 0.0;
-        double gruen = 0.0;
-        double blau = 0.0;
-        for (int x = halb; x < originalbild.getWidth() - halb; x++){ 
-            for (int y = halb; y < originalbild.getHeight() - halb; y++) {
-                int xx = x - halb;
-                int yy = y - halb;
-                pixelNeu[x][y] = new Color((int) rot, (int) gruen, (int) blau);
 
-                for (int i = 0; i < laenge; i++) { 
-                    for (int j = 0; j < laenge; j++) { 
-                        rot += filter[i][j] * pixel[xx+i][yy+j].getRed();
-                        gruen += filter[i][j] * pixel[xx+i][yy+j].getGreen();
-                        blau += filter[i][j] * pixel[xx+i][yy+j].getBlue();
-                        if(rot < 0.0) rot = 0.0;
-                        if(rot > 255.0) rot = 255.0;
-                        if(gruen < 0.0) gruen = 0.0;
-                        if(gruen > 255.0) gruen = 255.0;
-                        if(blau < 0.0) blau = 0.0;
-                        if(blau > 255.0) blau = 255.0;
-                    }
-                }
+
+        return faltung(originalbild,(double[][]) filter);
+
+     }
+    public Picture ____(Picture originalbild){
+        double filter[][] = new double [3][3];
+        double x =1.0/9.0;
+        filter [0][0] = x;
+        filter [1][0] = x;
+        filter [2][0] = x;
+        filter [0][1] = x;
+        filter [1][1] = x;
+        filter [2][1] = x;
+        filter [0][2] = x;
+        filter [1][2] = x;
+        filter [2][2] = x;
+        return faltung(originalbild,(double[][])filter);
+    }
+
+    public Picture weich(Picture originalbild){
+        int filtergroesse =3;
+        double filter [][] =new double [3][3];
+        double filtercontent = 1.0/9.0;
+        for(int i = 0; i<filtergroesse; i++){
+            for(int j=0; j<filtergroesse; j++){
+                filter[i][j] = filtercontent;
             }
         }
-        Picture neuesBild = new Picture();
+        return faltung(originalbild, (double[][])filter);
+    }
+    public Picture faltung(Picture originalbild,double[][] filter) {
+
+
+
+         Color[][] pixel = originalbild.getPixelArray();
+         Color[][] pixelNeu = originalbild.getPixelArray();
+         double rot = 0.0;
+         double gruen = 0.0;
+         double blau = 0.0;
+         int laenge = filter.length;
+         java.lang.System.out.println(laenge);
+         int halb = laenge / 2;
+
+          for (int x = halb; x < originalbild.getWidth() - halb; x++){
+               for (int y = halb; y < originalbild.getHeight() - halb; y++) {
+                  rot = 0.0;
+                  gruen = 0.0;
+                  blau = 0.0;
+
+                  for (int i = 0; i < laenge; i++) {
+                    for (int j = 0; j < laenge; j++) {
+                        int xx = x - halb;
+                        int yy = y - halb;
+
+
+                        rot += filter[i][j] * pixel[xx+i][yy+j].getRed(); 
+                        gruen += filter[i][j] * pixel[xx+i][yy+j].getGreen(); 
+                        blau += filter[i][j] * pixel[xx+i][yy+j].getBlue();
+
+                    }
+                    }
+                  if(rot < 0.0) rot = 0.0; 
+                  if(rot > 255.0) rot = 255.0;
+                  if(gruen < 0.0) gruen = 0.0; 
+                  if(gruen > 255.0) gruen = 255.0;
+                  if(blau < 0.0) blau = 0.0; 
+                  if(blau > 255.0) blau = 255.0; 
+
+
+                  pixelNeu[x][y] = new Color((int)rot,(int) gruen,(int) blau);
+
+
+
+                }
+ 
+          }
+        Picture neuesBild = new Picture(); 
         neuesBild.setPixelArray(pixelNeu);
         return neuesBild;
-    }
+   }
+    
 
     /**
      * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
@@ -67,5 +111,5 @@ public class Mehrpixeloperationen
      * @param  y    ein Beispielparameter für eine Methode
      * @return        die Summe aus x und y
      */
-    
+
 }

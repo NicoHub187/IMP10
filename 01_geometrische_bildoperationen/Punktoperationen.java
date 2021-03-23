bf385ae770ad9dd82d7c40d5ba87423f94b819e9
fimp10 <frediknapp05@gmail.com>
2021-03-23 11:17:52 +0100

    graustufenDurchschnitt

---
+++ 01_geometrische_bildoperationen/Punktoperationen.java
@@ -0,0 +1,57 @@
+import imp.*;
+import java.awt.Color;
+import java.util.Random;
+/**
+ * Beschreiben Sie hier die Klasse Punktoperationen.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Punktoperationen
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int x;
+
+    /**
+     * Konstruktor für Objekte der Klasse Punktoperationen
+     */
+    public Punktoperationen()
+    {
+        // Instanzvariable initialisieren
+        x = 0;
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        return 100 + 50;
+    }
+    
+     public int graustufenDurchschnitt(Picture originalbild){
+    
+        int breite = originalbild.getWidth();
+        int hoehe = originalbild.getHeight();
+        
+        Color[][] pixel = originalbild.getPixelArray();
+        Color[][] pixelNeu = new Color [breite][hoehe];
+        int c;
+        
+        for(int x=0; x < breite; x++) {
+            for(int y=0; y < hoehe; y++) {
+                c = (int) (pixel[x][y].getRed()+ pixel[x][y].getGreen()+ pixel[x][y].getBlue())/3;
+                pixelNeu[x][y] = new Color(c,c,c);
+            }
+        }
+        Picture neuesBild = new Picture();
+        neuesBild.setPixelArray(pixelNeu);
+        return neuesBild;
+        
+    
+    }
+}

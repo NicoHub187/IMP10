51c74024293811d056d262e157ee828debbb4fad
henriette123 <78789441+henriette123@users.noreply.github.com>
2021-03-23 11:06:58 +0100

    graustufen natürlich

--- 01_geometrische_bildoperationen/Punktoperationen.java
+++ 01_geometrische_bildoperationen/Punktoperationen.java
@@ -100,4 +100,34 @@ public class Punktoperationen
         neuesBild.setPixelArray(pixelNeu);
         return neuesBild;
     }
+    
+    public Picture graustufenNatuerlich(Picture originalbild){
+        int breite = originalbild.getWidth();
+        int hoehe = originalbild.getHeight();
+        
+        Color[][] pixel = originalbild.getPixelArray();
+        Color[][] pixelNeu = new Color[breite][hoehe];
+        for(int x=0; x < breite; x++) {
+            for(int y=0; y < hoehe; y++) {
+                int rotanteil = 0;
+                rotanteil = pixel[x][y].getRed;
+                int rotanteilNeu = rotanteil * 0.299;
+                
+                int gruenanteil = 0;
+                gruenanteil = pixel[x][y].getBlue;
+                int gruenanteilNeu = gruenanteil * 0.587;
+                
+                int blauanteil = 0;
+                blauanteil = pixel[x][y].getBlue;
+                int blauanteilNeu = blauanteil * 0.114;
+                
+                pixelNeu[x][y] = new Color (rotanteilNeu, gruenanteilNeu, blauanteilNeu);
+            }
+        }
+        
+        Picture neuesBild = new Picture();
+        neuesBild.setPixelArray(pixelNeu);
+        return neuesBild;
+    }
+    
 }

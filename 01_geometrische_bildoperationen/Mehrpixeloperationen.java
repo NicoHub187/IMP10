ae3334c4de769f20d66bd8ad23f8e13b11e12000
timp10 <timon.a.roeck@icloud.com>
2021-04-13 11:02:05 +0200

    Faltung 13.04

--- 01_geometrische_bildoperationen/Mehrpixeloperationen.java
+++ 01_geometrische_bildoperationen/Mehrpixeloperationen.java
@@ -1,3 +1,4 @@
+
 import imp.*;
 import java.awt.Color;
 /**
@@ -33,5 +34,26 @@ public class Mehrpixeloperationen
     }
     public Picture weichzeichnen (Picture originalbild) {
         
-    }      
+    }
+    public Picture faltung(Picture originalbild) { //double[][] filter
+         int filter [][] = new int [3][3];
+        filter [0][0] = -1;
+        filter [1][0] = -2;
+        filter [2][0] = -1;
+        filter [0][1] = 0;
+        filter [1][1] = 0;
+        filter [2][1] = 0;
+        filter [0][2] = 1;
+        filter [1][2] = 2;
+        filter [2][2] = 1;
+        
+        Color[][] pixel = originalbild.getPixelArray();
+       Color[][] pixelNeu = originalbild.getPixelArray();
+       double rot = 0.0;
+       double gruen = 0.0;
+       double blau = 0.0;
+       int laenge = filter.length; 
+       int halb = laenge / 2; 
+    }    
+
 }
